f2ed0b4e72f0662cc9933ad40e5d578ff1db74f6
Suin0730 <ei654028@gmail.com>
2021-04-24 00:51:14 +0900

    signin with naver

--- src/main/java/org/example/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/org/example/springboot/config/auth/dto/OAuthAttributes.java
@@ -5,7 +5,6 @@ import org.example.springboot.domain.user.Role;
 import org.example.springboot.domain.user.User;
 
 import java.util.Map;
-
 @Getter
 public class OAuthAttributes {
     private Map<String, Object> attributes;
@@ -24,6 +23,9 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
 
         return ofGoogle(userNameAttributeName, attributes);
     }
@@ -34,6 +36,18 @@ public class OAuthAttributes {
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
